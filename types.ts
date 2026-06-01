5fba59272c66a950b532f4a6d049c0548561d38a
xfazi-m <pxstudio.print@gmail.com>
2026-06-01 23:37:50 +0200

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,10 @@
+export interface PsdFile {
+  id: string;
+  name: string;
+  size: number;
+  previewUrl: string;
+  width: number;
+  height: number;
+  channels: number;
+  bitsPerChannel: number;
+}
